ea770afa25b5027f42263841246d5433e5ca0019
mellolian <braver93@mail.ru>
2020-04-27 16:27:54 +0300

    added gspread, not configured batch yet

--- HAS/spiders/HAS.py
+++ HAS/spiders/HAS.py
@@ -5,12 +5,25 @@ from scrapy.http import TextResponse
 import requests
 from pprint import pprint
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 
+scopes = ['https://spreadsheets.google.com/feeds',
+          'https://www.googleapis.com/auth/drive']
+
+credentials = Credentials.from_service_account_file(
+    'bmlweb-d5bced2a88c6.json', scopes=scopes)
 
 TAG_RE = re.compile(r'<[^>]+>')
 
 STRING_RE = re.compile(r'^((\s?)-|>)((.*)(20(1|2)\d))')
+
+gc = gspread.authorize(credentials)
+# Open a sheet from a spreadsheet in one go
+wks = gc.open("bmlweb").sheet1
+
+# Update a range of cells using the top left corner address
+wks.update('A1', [[1, 2], [3, 4]])
+
 
 
 def remove_tags(text):
@@ -39,16 +52,17 @@ class HASSpider(scrapy.Spider):
         for text in full_text:
             lines = text.split('<br>')
             for line in lines:
+                comment = {}
                 raw_line = remove_tags(line).replace(
                     '\n', '').replace('\t', '')
-
-                correct_line = (STRING_RE.findall(raw_line))
-                if not correct_line:
-                    continue
 
                 if 'href="' in line:
                     url = line.split('href="')[1].split('"')[0]
                     comment['url'] = url
+
+                correct_line = (STRING_RE.findall(raw_line))
+                if not correct_line:
+                    continue
 
                 online_data = correct_line[0][2].split('-')[-1]
                 if '–' in online_data:
@@ -66,4 +80,3 @@ class HASSpider(scrapy.Spider):
                         ' - ')[0].split(online_data)[0]
                 comment['title'] = title
                 comment['source'] = source
-
